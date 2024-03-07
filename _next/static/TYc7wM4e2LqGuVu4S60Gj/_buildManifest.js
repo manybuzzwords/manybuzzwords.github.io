13bf66b3effe978f272684ef71ba7baf70020dae
bespokestan <bespokestan@gmail.com>
2024-03-08 00:20:00 +0100

    test

---
+++ _next/static/TYc7wM4e2LqGuVu4S60Gj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/126-463631cda99964af.js","static/chunks/pages/index-2edd0a95dc017ada.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
